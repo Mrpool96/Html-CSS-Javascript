1e00aef91e3dae0ddb8829a91f8d900ae545b493
Mrpool96 <53513296+Mrpool96@users.noreply.github.com>
2019-10-12 16:56:57 +0530

    marks calculation using if and else

---
+++ task3.js
@@ -0,0 +1,11 @@
+function grade(marks){
+    var marks;
+    if (marks>40) {
+        console.log("Student is passed");
+        
+    } else {
+        console.log("Student is failed");
+        
+    }
+}
+grade(50)
