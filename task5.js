516ab3f2a5838d5658fb8fb67ce183eba20be134
Mrpool96 <53513296+Mrpool96@users.noreply.github.com>
2019-10-14 11:37:26 +0530

    Print table in a given range

--- task5.js
+++ task5.js
@@ -1,12 +1,11 @@
-function printTableRange(num){
-     var num;  
-        
-            for(i=1; i<=15; i++){
-            console.log(num+ "*" +i+ "=" +num*i);
-
-        } 
-    
-        
+  
+function tables(num1,num2){
+    for(var i=1;i<num2;i++){
+        for(var j=1;j<=10;j++){
+            var table=num1*j;
+            console.log(table);
+        }
+        num1++;
     }
-    printTableRange(13)
-    
+}
+tables(2,8)
