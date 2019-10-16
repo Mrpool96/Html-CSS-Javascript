0f6638d350dedfdd2bd17ce3a2d22bbabb6fa417
Mrpool96 <53513296+Mrpool96@users.noreply.github.com>
2019-10-16 08:07:34 +0530

    Word blanks

--- practice1.js
+++ practice1.js
@@ -1,3 +1,6 @@
-function wordBlanks(myNoun,myVerb,myAdjective,myAdverb){
-
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb){
+    var result="";
+    result += "The " +myAdjective+ "  " +myNoun+ "  " +myVerb+ " to his owner " +myAdverb;
+    return result;
 }
+    console.log(wordBlanks("dog","big","ran","quickly"));
