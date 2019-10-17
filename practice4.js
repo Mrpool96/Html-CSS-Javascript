85e7092639c55be5751425f61fcc5b523298f526
Mrpool96 <53513296+Mrpool96@users.noreply.github.com>
2019-10-17 14:26:51 +0530

    Cards

--- practice4.js
+++ practice4.js
@@ -2,7 +2,11 @@ function cards(card){
     var count=0;
     switch(card){
         case 2:
-        case
+        case 3:
+        case 4: 
+        case 5:
+        case 6:
+            count++
 
     }
 }
